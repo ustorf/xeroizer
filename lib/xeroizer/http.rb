947e5b3888d760ea56e3bc7623ccc6b451e71f81
Bernd Ustorf <bernd.ustorf@gmail.com>
2014-02-28 18:07:40 -0800

    handle 503

--- lib/xeroizer/http.rb
+++ lib/xeroizer/http.rb
@@ -94,6 +94,8 @@ module Xeroizer
               handle_oauth_error!(response)
             when 404
               handle_object_not_found!(response, url)
+            when 503
+              raise Xeroizer::OAuth::RateLimitExceeded
             else
               raise "Unknown response code: #{response.code.to_i}"
           end
